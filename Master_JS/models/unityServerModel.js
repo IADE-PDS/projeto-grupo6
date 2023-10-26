ae13c525610a1669c0c8f94c99540c04c09a6ea8
DuarteCavaleiro <99983370+DuarteCavaleiro@users.noreply.github.com>
2023-10-26 17:14:19 +0100

    slave deletion

--- Master_JS/models/unityServerModel.js
+++ Master_JS/models/unityServerModel.js
@@ -7,13 +7,13 @@ const client = db.getdatabase();
 
 function dbLobbiestoLobby(dbr)  {
     return new UnityServer(dbr.id, dbr.match_id, dbr.max_players, dbr.games_pool,
-        dbr.isPrivate, dbr.game_name, dbr.isOfficial, dbr.port, dbr.slave, dbr.status);
+        dbr.isPrivate, dbr.game_name, dbr.isOfficial, dbr.port, dbr.child, dbr.status);
 }
 
 
 class UnityServer {
     constructor(id,match_id, max_players, games_pool, isPrivate, game_name,
-        isOfficial, port, slave, status) {
+        isOfficial, port, child, status) {
         this.id = id;
         this.match_id= match_id;
         this.max_players = max_players;
@@ -22,7 +22,7 @@ class UnityServer {
         this.game_name = game_name;
         this.isOfficial = isOfficial;
         this.port = port;
-        this.slave = slave;
+        this.child = child;
         this.status = status;
     }
     export() {
@@ -33,7 +33,7 @@ class UnityServer {
         unityserver.game_name = this.game_name;
         unityserver.isOfficial = this.isOfficial;
         unityserver.port = this.port;
-        unityserver.slave = this.slave;
+        unityserver.child = this.child;
         unityserver.status = this.status;     
         return unityserver; 
     }
