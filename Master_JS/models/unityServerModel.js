3333c528079fdd0dcd480c0647418cd0e7badb88
DuarteCavaleiro <99983370+DuarteCavaleiro@users.noreply.github.com>
2023-10-26 17:09:40 +0100

    search lobbies

--- Master_JS/models/unityServerModel.js
+++ Master_JS/models/unityServerModel.js
@@ -6,31 +6,35 @@ const client = db.getdatabase();
 
 
 function dbLobbiestoLobby(dbr)  {
-    return new UnityServer(dbr.id,
-        dbr.server_namename,dbr.pass, dbr.token);
+    return new UnityServer(dbr.id, dbr.match_id, dbr.max_players, dbr.games_pool,
+        dbr.isPrivate, dbr.game_name, dbr.isOfficial, dbr.port, dbr.slave, dbr.status);
 }
 
 
 class UnityServer {
-    constructor(id, max_players, games_pool, isPrivate, isOfficial,
-         server_name, port, slave, status) {
+    constructor(id,match_id, max_players, games_pool, isPrivate, game_name,
+        isOfficial, port, slave, status) {
         this.id = id;
+        this.match_id= match_id;
         this.max_players = max_players;
         this.games_pool = games_pool;
         this.isPrivate = isPrivate;
+        this.game_name = game_name;
         this.isOfficial = isOfficial;
-        this.server_name = server_name;
         this.port = port;
         this.slave = slave;
         this.status = status;
     }
     export() {
         let unityserver=new UnityServer();
-        unityserver.server_namename = this.server_name;
-        unityserver.slave = this.slave;
-        unityserver.status = this.status;
+        unityserver.match_id= this.match_id;
+        unityserver.max_players = this.max_players;
         unityserver.isPrivate = this.isPrivate;
+        unityserver.game_name = this.game_name;
+        unityserver.isOfficial = this.isOfficial;
         unityserver.port = this.port;
+        unityserver.slave = this.slave;
+        unityserver.status = this.status;     
         return unityserver; 
     }
 
@@ -39,6 +43,7 @@ class UnityServer {
             let lobbies= [];
             let results= await client.collection("lobby")
                 .find({isPrivate: false,
+                       isOfficial: false,
                        status: "waiting"})
                 .sort({server_name: 1})
                 .toArray()
