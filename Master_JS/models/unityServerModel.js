db1228f0f0bffccc639a03fc6963f5fea4d2d956
DuarteCavaleiro <99983370+DuarteCavaleiro@users.noreply.github.com>
2023-10-27 23:26:04 +0100

    CreatePublicServerModel
    model da função CreatePublicserver

--- Master_JS/models/unityServerModel.js
+++ Master_JS/models/unityServerModel.js
@@ -6,15 +6,14 @@ const client = db.getdatabase();
 
 
 function dbLobbiestoLobby(dbr)  {
-    return new UnityServer(dbr.id, dbr.match_id, dbr.max_players, dbr.games_pool,
+    return new UnityServer(dbr.match_id, dbr.max_players, dbr.games_pool,
         dbr.isPrivate, dbr.game_name, dbr.isOfficial, dbr.port, dbr.child, dbr.status);
 }
 
 
 class UnityServer {
-    constructor(id,match_id, max_players, games_pool, isPrivate, game_name,
+    constructor(match_id, max_players, games_pool, isPrivate, game_name,
         isOfficial, port, child, status) {
-        this.id = id;
         this.match_id= match_id;
         this.max_players = max_players;
         this.games_pool = games_pool;
@@ -38,10 +37,10 @@ class UnityServer {
         return unityserver; 
     }
 
-    static async getAllLobbies() {
+    static async GetAllLobbies() {
         try {
             let lobbies= [];
-            let results= await client.collection("lobby")
+            let results= await client.collection("unity")
                 .find({isPrivate: false,
                        isOfficial: false,
                        status: "waiting"})
@@ -58,6 +57,52 @@ class UnityServer {
         }  
     }
 
+      
+    
+        
+        
+    static async CreateUnityServerPublic( max_players, game_name,
+        games_pool, match_id, port, child) {
+        try {
+            let db = client.collection("unity")
+            //missing email verification
+            if(!game_name || !match_id || !port || !child)
+                return {status: 422,result: {
+                    msg:"Bad Data"
+                }}
+            if(!games_pool || games_pool==[] ){ games_pool="all"}
+            if(max_players<=0){max_players=4}
+            
+            let dbResult = await db.find({ game_name: game_name}).toArray();
+            if(dbResult.length){
+                return {status: 400, result: {
+                    msg:"this name already exists"
+                }}
+            };
+                 
+            let insert_unityserver = new UnityServer();
+            insert_unityserver.isPrivate = false;
+            insert_unityserver.max_players = max_players;
+            insert_unityserver.game_name = game_name;
+            insert_unityserver.games_pool = games_pool;
+            insert_unityserver.isOfficial = false;
+            insert_unityserver.match_id = match_id;
+            insert_unityserver.match_id = port;
+            insert_unityserver.child = child;
+            insert_unityserver.status = "waiting";
+
+            dbResult = await db.insertOne(insert_unityserver);
+            let unityserver_id = dbResult.insertedId;
+            return {status: 200, result: {
+                msg:"unity server created sucessfully"
+            }}
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: { msg: "Internal server error" }};
+        }  
+    }
+
 }
+
 
 module.exports = UnityServer;
